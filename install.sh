daea38dbdbcfc8d41d7765f1e2c98c2df09524dc
bradm <git@bradm.io>
2023-04-21 16:19:31 +0930

    Added install file for quicker recompilation, and updated perf_counters_util.h to use pread() instead of fgets()

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Nasty Hardcoding
+if [[ $(lscpu | grep -c "Intel(R) Core(TM)") ]]; then
+	TYPE="INTEL_CORE"
+else
+	TYPE="INTEL_XEON"
+fi
+
+#even more nasty hardcoding
+MODEL=$(lscpu | grep "Intel" | awk -F "Intel" '{print $2}' | awk -F '-' '{print $2}')
+MODEL=$(echo $MODEL | head -c 2)
+echo $MODEL
+GEN="GEN"
+if [[ $MODEL -ge 20 ]]; then
+	MODEL=$(echo $MODEL | head -c 1)
+fi
+GEN=$GEN$MODEL
+
+echo $TYPE
+echo $GEN
+echo "Compiling..."
+
+make all OPS="-D$TYPE -D$GEN"
+
+echo "Test with 'sudo ./test_pmu' and 'sudo ./test_uncore'"
